de7fd43d3d624adbdcb4b07caf88604b14494629
Nicholas Anich <NicholasAnich.infinitevolume@gmail.com>
2022-09-19 23:56:29 -0700

    fixed blank screen issue by removing unused code in app.js

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
-import { createAvatar } from "@dicebear/avatars";
-import * as style from "@dicebear/big-smile";
 
-let svg = createAvatar(style, {
-  seed: "custom-seed",
-  
-});
 
-const myImgs = document.querySelectorAll("img");
 
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    console.log(entry);
     if (entry.isIntersecting) {
       entry.target.classList.add("show");
     } else {
